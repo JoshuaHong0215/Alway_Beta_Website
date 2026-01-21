e9390972068b21e5404575c4cb869ed0e3fa2455
JoshuaHong0215 <JoshuaHong0215>
2026-01-21 17:49:22 +0900

    data구조 세분화 작업 진행예정

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -34,20 +34,27 @@ export const projects: ProjectItem[] = [
       대지면적: 1,072,00㎡
     `,
     image: 'songdo_Paulbasset.jpeg',
-    tags: ['Architecture', 'Sustainable', 'Concept'],
+    tags: ['Architecture', 'Sketchup', 'AutoCAD', 'Lumion', 'Photoshop'],
     date: '2023. 11. 10',
     
     sections: [
       {
-        category: 'Before',
-        title: '공사도면 및 지형정보 확인',
+        category: 'Rendering',
+        title: '투시도',
+        description: `공사평면도와 국토지리정보원도면과 더블체크 이후 QGIS로 지형정보 정제`,
+        image: 'songdo_Paulbasset.jpeg'  
+      },
+
+      {
+        category: '',
+        title: '공사 전',
         description: `공사평면도와 국토지리정보원도면과 더블체크 이후 QGIS로 지형정보 정제`,
         image: 'songdoBefore.png'  
       },
 
       {
-        category: 'After',
-        title: 'PCG 디자인',
+        category: '',
+        title: '공사 후',
         description: `PCG 설계를 통하여 넓은 환경에 고퀄리티 Asset을 효율적으로 배치하였음
         PCG 설계를 통하여 저용량, 고효율의 작업성을 확보하였음`,
         image: 'songdoAfter.png'
